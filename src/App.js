d39bc1768fbe87393ef3329622aea71a18cf4e7c
AISWARIA-A <aiswariaanilkumar@gmail.com>
2023-06-28 00:15:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/header/footer/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
